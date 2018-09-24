3a23e752fe2df51c715037b942f62cc382072f80
Damian Stasik <visualfanatic@users.noreply.github.com>
2018-09-24 15:42:32 +0200

    Core code

---
+++ index.js
@@ -0,0 +1,53 @@
+const SVGO = require('svgo');
+const { compile } = require('vue-template-compiler');
+const stripWith = require('vue-template-es2015-compiler');
+
+module.exports = (content, options = {}) => {
+  const {
+    svgoConfig = {},
+    svgoPath = null,
+  } = options;
+
+  let svg = Promise.resolve(content);
+
+  if (svgoConfig !== false) {
+    svg = new SVGO(svgoConfig)
+      .optimize(content, { path: svgoPath })
+      .then(result => result.data);
+  }
+
+  return svg.then((result) => {
+    let { render: renderFunction } = compile(result, {
+      preserveWhitespace: false,
+      modules: [
+        {
+          transformNode: (el) => {
+            if (el.tag === 'svg') {
+              el.classBinding = '[data.class, data.staticClass]';
+              el.styleBinding = '[data.style, data.staticStyle]';
+            }
+          },
+        },
+      ],
+    });
+
+    renderFunction = `
+      function render(_h, _vm) {
+        ${renderFunction};
+      }
+    `;
+
+    renderFunction = stripWith(renderFunction, {
+      transforms: {
+        stripWithFunctional: true,
+      },
+    });
+
+    return `
+      module.exports = {
+        functional: true,
+        render: ${renderFunction},
+      };
+    `;
+  });
+};

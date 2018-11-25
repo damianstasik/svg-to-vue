36ad5a595d57e38422d2576960c36a2e0cf730e5
Damian Stasik <visualfanatic@users.noreply.github.com>
2018-11-25 23:19:44 +0100

    Rewrite component render function to support providing attributes other than clas and style

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 const SVGO = require('svgo');
 const { compile } = require('vue-template-compiler');
-const stripWith = require('vue-template-es2015-compiler');
+
+const transformChildren = (value) => {
+  const chilldren = value.reduce((acc, child) => {
+    if (child.text) {
+      acc.push(`_v('${child.text}')`);
+    } else {
+      const args = [`'${child.tag}'`];
+
+      if (Object.keys(child.attrsMap).length) {
+        const data = [];
+
+        if (child.staticClass) {
+          data.push(`staticClass:${child.staticClass}`);
+        }
+
+        if (child.staticStyle) {
+          data.push(`staticStyle:${child.staticStyle}`);
+        }
+
+        if (child.attrsList.length) {
+          const attrs = child.attrsList.reduce((v, attr) => ({
+            ...v,
+            [attr.name]: attr.value,
+          }), {});
+
+          data.push(`attrs:${JSON.stringify(attrs)}`);
+        }
+
+        if (data.length) {
+          args.push(`{${data.join()}}`);
+        }
+      }
+
+      if (child.children.length) {
+        args.push(transformChildren(child.children));
+      }
+
+      acc.push(`_c(${args.join()})`);
+    }
+
+    return acc;
+  }, []);
+
+  return `[${chilldren.join()}]`;
+};
 
 module.exports = (content, options = {}) => {
   const {
@@ -17,36 +61,36 @@ module.exports = (content, options = {}) => {
   }
 
   return svg.then((result) => {
-    let { render: renderFunction } = compile(result, {
+    const { ast } = compile(result, {
       preserveWhitespace: false,
-      modules: [
-        {
-          transformNode: (el) => {
-            if (el.tag === 'svg') {
-              el.classBinding = '[data.class, data.staticClass]';
-              el.styleBinding = '[data.style, data.staticStyle]';
-            }
-          },
-        },
-      ],
-    });
-
-    renderFunction = `
-      function render(_h, _vm) {
-        ${renderFunction}
-      }
-    `;
-
-    renderFunction = stripWith(renderFunction, {
-      transforms: {
-        stripWithFunctional: true,
-      },
     });
 
     return `
       export default {
         functional: true,
-        render: ${renderFunction}
+        render(_h, _vm) {
+          const { _c, _v, data } = _vm;
+
+          const {
+            class: classNames,
+            staticClass,
+            style,
+            staticStyle,
+            attrs = {},
+            ...rest
+          } = data;
+
+          return _c(
+            'svg',
+            {
+              class: [${ast.staticClass}, classNames, staticClass],
+              style: [${ast.staticStyle}, style, staticStyle],
+              attrs: Object.assign(${JSON.stringify(ast.attrsMap)}, attrs),
+              ...rest,
+            },
+            ${transformChildren(ast.children)}
+          )
+        }
       }
     `;
   });

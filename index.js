bdd58b226d9214fc2ee8b5b946f09670a10b4f68
Adrien Foulon <6115458+Tofandel@users.noreply.github.com>
2020-06-29 17:38:33 +0200

    Make use of vue-template-compiler to avoid the need for babel

--- index.js
+++ index.js
@@ -1,52 +1,5 @@
 const SVGO = require('svgo');
-const { compile } = require('vue-template-compiler');
-
-const transformChildren = (value) => {
-  const chilldren = value.reduce((acc, child) => {
-    if (child.text) {
-      acc.push(`_v('${child.text}')`);
-    } else {
-      const args = [`'${child.tag}'`];
-
-      if (Object.keys(child.attrsMap).length) {
-        const data = [];
-
-        if (child.staticClass) {
-          data.push(`staticClass:${child.staticClass}`);
-        }
-
-        if (child.staticStyle) {
-          data.push(`staticStyle:${child.staticStyle}`);
-        }
-
-        if (child.attrsList.length) {
-          const attrs = child.attrsList.reduce((v, attr) => ({
-            ...v,
-            [attr.name]: attr.value,
-          }), {});
-
-          data.push(`attrs:${JSON.stringify(attrs)}`);
-        }
-
-        if (data.length) {
-          args.push(`{${data.join()}}`);
-        }
-      }
-
-      if (child.children.length) {
-        args.push(transformChildren(child.children));
-      }
-
-      acc.push(`_c(${args.join()})`);
-    }
-
-    return acc;
-  }, []);
-
-  return `[${chilldren.join()}]`;
-};
-
-const stringify = (value) => value.filter((item) => item).join();
+const { parseComponent } = require('vue-template-compiler');
 
 module.exports = (content, options = {}) => {
   const {
@@ -63,50 +16,6 @@ module.exports = (content, options = {}) => {
   }
 
   return svg.then((result) => {
-    const { ast } = compile(result, {
-      preserveWhitespace: false,
-    });
-
-    const children = ast.children.length
-      ? `children.concat(${transformChildren(ast.children)})`
-      : 'children';
-
-    delete ast.attrsMap.class;
-
-    const attrs = Object.keys(ast.attrsMap).length
-      ? `attrs: Object.assign(${JSON.stringify(ast.attrsMap)}, attrs)`
-      : 'attrs';
-
-    const classNames = stringify([ast.staticClass, 'classNames', 'staticClass']);
-    const styles = stringify([ast.staticStyle, 'style', 'staticStyle']);
-
-    return `
-      module.exports = {
-        functional: true,
-        render(_h, _vm) {
-          const { _c, _v, data, children = [] } = _vm;
-
-          const {
-            class: classNames,
-            staticClass,
-            style,
-            staticStyle,
-            attrs = {},
-            ...rest
-          } = data;
-
-          return _c(
-            'svg',
-            {
-              class: [${classNames}],
-              style: [${styles}],
-              ${attrs},
-              ...rest,
-            },
-            ${children}
-          )
-        }
-      }
-    `;
+    return parseComponent('<template functional>' + file.replace('<svg', '<svg v-bind="data.attrs" :class="data.staticClass" :style="data.staticStyle"') + '</template>', options);
   });
 };
